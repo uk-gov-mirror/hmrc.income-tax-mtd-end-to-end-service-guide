2f4e9806bc3e39047953c26aaeb10c2882ce0396
Narendra Vijayarao <26090262+narendravijayarao@users.noreply.github.com>
2020-03-26 14:56:49 +0000

    Revert upgrade changes (#254)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,13 +3,13 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "1.5.0"
+  val bootStrapPlayVersion = "5.1.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-26" % bootStrapPlayVersion)
+    "uk.gov.hmrc" %% "bootstrap-play-25" % bootStrapPlayVersion)
 
   val test = Seq(
     "org.pegdown" % "pegdown" % "1.6.0" % "test",
-    "org.scalatest" %% "scalatest" % "3.1.0" % "test")
+    "org.scalatest" %% "scalatest" % "3.0.5" % "test")
 }
