04b20af71b1c819f6ddfe298904dc6892d534fbf
Yang Wong <20066247+yangwong9664@users.noreply.github.com>
2020-11-06 13:49:14 +0000

    fix compile issue (#390)

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,12 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "5.1.0"
+
+  val bootStrapPlayVersion = "2.0.0"
 
   val compile = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-play-25" % bootStrapPlayVersion)
+    "uk.gov.hmrc" %% "bootstrap-play-26" % bootStrapPlayVersion)
 
   val test = Seq(
     "org.pegdown" % "pegdown" % "1.6.0" % "test",
