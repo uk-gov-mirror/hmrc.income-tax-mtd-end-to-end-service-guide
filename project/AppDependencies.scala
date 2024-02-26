4c778ddbefcdc8e60d69c29bdef25eb991eff877
Michael Olafisoye <86134318+molafisoye@users.noreply.github.com>
2024-02-26 11:32:45 +0000

    MTDSA-21270 updated AppDependencies (#747)
    * MTDSA-21270 updated AppDependencies
    
    * MTDSA-21270 updated AppDependencies with sbt version
    
    * MTDSA-21270 remove unused config
    
    * MTDSA-21270 update sbt-distributables and remove unused config
    
    * MTDSA-21270 update 4.8.0
    
    * MTDSA-21270 update 3.5.0

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -3,12 +3,12 @@ import sbt._
 
 object AppDependencies {
 
-  val bootStrapPlayVersion = "7.11.0"
-  val compile              = Seq(ws, "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
+  val bootStrapPlayVersion = "8.0.0"
+  val compile              = Seq(ws, "uk.gov.hmrc" %% "bootstrap-frontend-play-30" % bootStrapPlayVersion)
 
   val test = Seq(
     "org.scalatest"       %% "scalatest"              % "3.2.15"             % "test",
-    "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootStrapPlayVersion % "test",
+    "uk.gov.hmrc"         %% "bootstrap-test-play-30" % bootStrapPlayVersion % "test",
     "com.vladsch.flexmark" % "flexmark-all"           % "0.64.6"             % "test"
   )
 
